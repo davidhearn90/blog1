e288763175ada6ceaa6ec1e5ebb44e651021f430
David Hearn <davidhearn90@gmail.com>
2014-11-03 10:26:48 -0800

    header and footer

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<?php
 
- require_once(__DIR__ . "/controller/create-db.php");
+<?php
+require_once(__DIR__ . "/view/header.php");    
+require_once(__DIR__ . "/controller/create-db.php");
+require_once(__DIR__ . "/view/footer.php");
 ?>
 
-</body>
-</html>	
