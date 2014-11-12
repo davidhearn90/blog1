e1a5bd533a6ca6c7a813c7115338bf902f66a6ff
David Hearn <davidhearn90@gmail.com>
2014-11-12 08:59:15 -0800

    establishing a database connection

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,9 +1,16 @@
 <?php
+require_once(__DIR__ . "/../model/database.php");
+//creates a query yhat connects to the database
+$connection = new mysqli($host, $username, $password, $database);
+
 //sanitizes $title and $post
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 //echos title and post
 echo"<p>Title: $title</p>";
 echo"<p>Post: $post</p>";
+
+  $connection->close();
+
 
 
