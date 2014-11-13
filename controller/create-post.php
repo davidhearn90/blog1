bf7b8341b664ff31de2f02c6f50ab189affb0d59
David Hearn <davidhearn90@gmail.com>
2014-11-13 09:10:54 -0800

    querying the database to submit a blog post

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,16 +1,26 @@
 <?php
 require_once(__DIR__ . "/../model/database.php");
-//creates a query yhat connects to the database
+//creates a query that connects to the database
 $connection = new mysqli($host, $username, $password, $database);
 
 //sanitizes $title and $post
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-//echos title and post
-echo"<p>Title: $title</p>";
-echo"<p>Post: $post</p>";
+//inserts the values of title and post in the table post
+$query = $connection->query("INSERT INTO posts SET title = '$title',post = '$post' ");
 
+if($query){
+	//runs if query is true
+	echo "<p>Successfully inserted post: $title </p>";
+}
+
+else{
+	//runs if query is true
+	echo "<p> $connection->error </p>";
+
+}
   $connection->close();
+
 
 
 
