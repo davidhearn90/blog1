f2028d9e30b6a6c273cb33dc7a572de022835f37
David Hearn <davidhearn90@gmail.com>
2014-11-17 10:20:47 -0800

    creating a constructor for our database class

---
+++ model/Database.php
@@ -0,0 +1,19 @@
+<?php
+//creates your own object so you can call these functions easier
+class Database{
+//private so other files can't access it
+private $connection;
+private $host;
+private $username;
+private $password;
+private $database;
+//assigns information to the global variables
+public function __construct($host, $username, $password, $database){
+	$this->host = $host;
+	$this->host = $username;
+	$this->host = $password;
+	$this->host = $database;
+
+}
+	
+}
