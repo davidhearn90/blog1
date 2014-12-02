2d00e0d7e2b53483d8227e8e7f89bf9d9db08feb
David Hearn <davidhearn90@gmail.com>
2014-12-02 08:50:13 -0800

    creating a registration form

---
+++ view/register-form.php
@@ -0,0 +1,26 @@
+<form>
+<div>
+<!-- label for email -->
+<label for="email">Email:</label>
+<!-- inputs the email as text -->
+<input type="text" name="email"/>
+</div>
+
+<div>
+<!-- label for username -->
+	<label for="username">Username:</label>
+    <input type="text" name="username"/>
+ <!-- inputs the username as text -->
+</div>
+
+<div>
+<!-- label for password -->
+	<label for="password">Password:</label>
+	<input type="password" name="password"/>
+	<!-- inputs the password as password -->
+</div>
+
+<div>
+	<button type="submit">Submit</button>
+</div>	
+</form>
